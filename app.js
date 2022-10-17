d5c32ab5f23d49f88f5dddf08159375d50a8d216
Zola <zolboo@alumni.stanford.edu>
2022-10-17 15:27:02 -0700

    temperature data

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ async function draw() {
   const dataset = await d3.json("./data.json");
   console.log(dataset[0]);
 
+  const xAccessor = (d) => {
+    return d.currently.humidity;
+  };
+
+  const yAccessor = (d) => {
+    return d.currently.apparentTemperature;
+  };
+
   //Dimensions
   let dimensions = {
     width: 800,
@@ -14,6 +22,11 @@ async function draw() {
       right: 50,
     },
   };
+
+  dimensions.ctrWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.ctrHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.left;
 
   const svg = d3
     .select("#chart")
@@ -28,6 +41,18 @@ async function draw() {
       `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
     );
 
+  // Scales
+  const xScales = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .rangeRound([0, dimensions.ctrWidth])
+    .clamp(true);
+  const yScales = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .rangeRound([dimensions.ctrHeight, 0])
+    .nice()
+    .clamp(true);
   //Draw Circles
   ctr
     .selectAll("circle")
@@ -36,15 +61,46 @@ async function draw() {
       return enter
         .append("circle")
         .attr("r", (d) => {
-          // console.log(d);
-          return d.currently.humidity;
+          return 5;
         })
-        .attr("cx", (d) => {
-          return d.currently.ozone;
-        })
-        .attr("cy", (d) => {
-          return d.currently.temperature;
-        });
+        .attr("cx", (d) => xScales(xAccessor(d)))
+        .attr("cy", (d) => yScales(yAccessor(d)))
+        .attr("data-temp", yAccessor);
+    })
+    .attr("fill", "red");
+
+  //Draw Axes
+  const xAxes = d3
+    .axisBottom(xScales)
+    .ticks(5)
+    .tickFormat((d) => {
+      return d * 100 + "%";
     });
+  //   const yAxes = d3.
+
+  const xAxisGroup = ctr
+    .append("g")
+    .call(xAxes)
+    .style("transform", `translateY(${dimensions.ctrHeight}px)`)
+    .classed("axis", true);
+
+  xAxisGroup
+    .append("text")
+    .text("Humidity")
+    .attr("fill", "black")
+    .attr("x", dimensions.ctrWidth / 2)
+    .attr("y", dimensions.margin.bottom - 10);
+
+  const yAxis = d3.axisLeft(yScales);
+  const yAxisGroup = ctr.append("g").call(yAxis).classed("axis", true);
+  yAxisGroup
+    .append("text")
+    .style("transform", "rotate(270deg)")
+    .attr("x", -dimensions.ctrHeight / 2)
+    .attr("y", -dimensions.margin.left + 15)
+    .style("text-anchor", "middle")
+    .attr("fill", "black")
+    .html("Temperature &deg; F");
 }
+
 draw();
